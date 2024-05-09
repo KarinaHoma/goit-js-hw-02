63deaa6a6735bc737705b6ef61c7e0cc63cd8d13
KarinaHoma <karina.homa0@gmail.com>
2024-05-09 18:36:56 +0100

    task 3

--- task-3.js
+++ task-3.js
@@ -1,1 +1,13 @@
+function checkForSpam(message) {
+  const newMessage = message.toLowerCase();
 
+  if (newMessage.includes(`sale`) || newMessage.includes(`spam`)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(checkForSpam("Hurry up, buy now!")); // true
+console.log(checkForSpam("Great deals in our sale!")); // true
+console.log(checkForSpam("Hello, how are you?")); // false
